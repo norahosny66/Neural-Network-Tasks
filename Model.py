10c087b080c6880474d31e4fee1e562a4d2f0502
norahosny66 <48599241+norahosny66@users.noreply.github.com>
2022-04-09 22:48:56 +0200

    Change Activation functions

--- Model.py
+++ Model.py
@@ -8,50 +8,36 @@ class AdalineModel:
         self.weight = np.random.rand(3, 1)
         if not isBias:
             self.weight[0] = 0
-        while Epochs != 0:
+        for i in range(Epochs):
             i = 0
             for x1, x2 in x:
-
-                v = self.weight[0][0] + self.weight[1][0] * x1 + self.weight[2][0] * x2
-                if v <0:
-                    v = 0
-                else:
-                    v = 1
-                error = y[i][0]-v
-
+                v = self.weight[0] +( self.weight[1] * x1) + self.weight[2] * x2
+                error = y[i]-v
                 if isBias:
                     self.weight[0] = self.weight[0] + error * learningRate
-                self.weight[1] = self.weight[1] + error * learningRate * x1
-                self.weight[2] = self.weight[2] + error * learningRate * x2
+                self.weight[1] = self.weight[1] + (error * learningRate * x1)
+                self.weight[2] = self.weight[2] +(error * learningRate * x2)
                 i += 1
 
             sum = 0
             i=0
             for x1, x2 in x:
-                v = self.weight[0][0] + self.weight[1][0] * x1 + self.weight[2][0] * x2
-                if v < 0:
-                    v = 0
-                else:
-                    v = 1
-                error = y[i][0]-v
+                v = self.weight[0] + (self.weight[1] * x1) + self.weight[2] * x2
+                error = y[i]-v
                 sum += (error * error)
                 i += 1
             Epochs-=1
             msq = sum / (2 * x.shape[0])
             print(msq,x.shape[0])
-            if msq < Threshold:
+            if msq <= Threshold:
                 break
 
 
     def predict(self , x):
         y = []
         for x1, x2 in x:
-            v = self.weight[0][0] + self.weight[1][0] * x1 + self.weight[2][0] * x2
-            if v < 0:
-                y.append(0)
-            else:
-                y.append(1)
-
+            v = self.weight[0] + (self.weight[1]* x1 )+ self.weight[2]* x2
+            y.append(np.sign(v))
         return y
 
     def ConfusionMatrix(self, Y, y_pred):
@@ -62,16 +48,16 @@ class AdalineModel:
         for i in range(len(Y)):
             if Y[i][0] == 1 and y_pred[i] == 1:
                 TP += 1
-            elif Y[i][0] == 0 and y_pred[i] == 1:
+            elif Y[i][0] == -1 and y_pred[i] == 1:
                 FP += 1
-            elif Y[i][0] == 1 and y_pred[i] == 0:
+            elif Y[i][0] == 1 and y_pred[i] == -1:
                 FN += 1
-            elif Y[i][0] == 0 and y_pred[i] == 0:
+            elif Y[i][0] == -1 and y_pred[i] == -1:
                 TN += 1
         return TP, FP, TN, FN
 
     def GetPoints(self, X):
         Y = []
         for x1 in X:
-            Y.append((-self.weight[0] - self.weight[1] * x1) / self.weight[2])
+            Y.append( (-self.weight[0] - (self.weight[1] * x1))/ self.weight[2] )
         return Y
